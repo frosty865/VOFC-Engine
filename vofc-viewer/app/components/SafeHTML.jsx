8c6b22a2d1fcf13ad2ca2c43ee257b354adc7547
Matthew Frost <frostech@gmail.com>
2025-10-18 12:10:42 -0400

    Fix SafeHTML component and VulnerabilityCard to resolve OFC display issues
    - Removed DOMPurify dependency from SafeHTML to avoid jsdom conflicts
    - Implemented simple HTML sanitization without external dependencies
    - Fixed VulnerabilityCard to use sector_id instead of sector field
    - This should resolve the 500 error preventing OFCs from displaying
    - Components should now render properly without jsdom/DOMPurify issues

--- vofc-viewer/app/components/SafeHTML.jsx
+++ vofc-viewer/app/components/SafeHTML.jsx
@@ -1,5 +1,4 @@
 import { memo } from 'react';
-import { SecurityUtils } from '../../lib/security';
 
 /**
  * Safe HTML component that sanitizes content to prevent XSS attacks
@@ -9,7 +8,14 @@ const SafeHTML = memo(({ content, className, ...props }) => {
     return null;
   }
 
-  const sanitizedContent = SecurityUtils.sanitizeHTML(content);
+  // Simple HTML sanitization without DOMPurify to avoid jsdom conflicts
+  const sanitizedContent = content
+    .replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '') // Remove script tags
+    .replace(/<iframe\b[^<]*(?:(?!<\/iframe>)<[^<]*)*<\/iframe>/gi, '') // Remove iframe tags
+    .replace(/on\w+="[^"]*"/gi, '') // Remove event handlers
+    .replace(/javascript:/gi, '') // Remove javascript: URLs
+    .replace(/data:/gi, '') // Remove data: URLs
+    .replace(/vbscript:/gi, ''); // Remove vbscript: URLs
 
   return (
     <div 
